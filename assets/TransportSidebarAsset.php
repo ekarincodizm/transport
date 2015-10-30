0fba6ce57261caab3dbb7bacee908ae9fdc1f3c6
kimniyom <kimniyomclub@gmail.com>
2015-10-30 20:23:14 +0700

    new commit

--- assets/TransportSidebarAsset.php
+++ assets/TransportSidebarAsset.php
@@ -14,20 +14,23 @@ use yii\web\AssetBundle;
  * @author Qiang Xue <qiang.xue@gmail.com>
  * @since 2.0
  */
-class TransportAsset extends AssetBundle {
+class TransportSidebarAsset extends AssetBundle {
 
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'web/themes/transport/css/system.css',
-        'web/themes/transport/css/bootstrap.css',
-        
-        'web/themes/transport/font-awesome/css/font-awesome.css',
+        'web/themes/transport-sidebar/css/bootstrap.css',
+        'web/themes/transport-sidebar/css/animate.min.css',
+        'web/themes/transport-sidebar/css/light-bootstrap-dashboard.css',
+        'web/themes/transport-sidebar/css/pe-icon-7-stroke.css',
+        'web/themes/transport-sidebar/font-awesome/css/font-awesome.css',
         'web/croppic-master/assets/css/main.css',
         'web/croppic-master/assets/css/croppic.css',
     ];
     public $js = [
-        'web/themes/transport/js/bootstrap.js',
+        'web/themes/transport-sidebar/js/bootstrap.min.js',
+        'web/themes/transport-sidebar/js/bootstrap-notify.js',
+        'web/themes/transport-sidebar/js/light-bootstrap-dashboard.js',
         'web/croppic-master/assets/js/jquery.mousewheel.min.js',
         'web/croppic-master/croppic.min.js',
     ];
