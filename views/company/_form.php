669c6f3920c99e0007ce598ee8971e0701befc4a
mashimaro <amnat_kalwtunkum@hotmail.com>
2015-11-16 17:33:28 +0700

    สร้าง ข้อมูลบริษัท

---
+++ views/company/_form.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Company */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="company-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'CompanyName')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Address')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'contact')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'Taxation_Number')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
