62076dde340883248d85b6a99f193c5d9c1115e5
mashimaro2010 <amnat_kalwtunkum@hotmail.com>
2015-12-16 11:05:41 +0700

    เพิ่มบัญชีธนาคาร

---
+++ views/Account/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+/*use yii\widgets\ActiveForm;*/
+use kartik\form\ActiveForm;
+/*use kartik\date\DatePicker;*/
+
+/* @var $this yii\web\View */
+/* @var $model app\models\Account */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="account-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'id')->textInput() ?>
+
+    <?= $form->field($model, 'account_number')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'account_name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'saving_type')->textInput() ?>
+
+    <?= $form->field($model, 'bank_name')->textInput() ?>
+
+    <?= $form->field($model, 'status')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
