f45355352de46cdd9ee0d880cd67d0d5cc06bc1d
kimniyom <kimniyomclub@gmail.com>
2015-12-24 13:32:35 +0700

    new commit

--- views/map-truck/view.php
+++ views/map-truck/view.php
@@ -1,5 +1,6 @@
-<?php 
-    use yii\helpers\Url;
+<?php
+
+use yii\helpers\Url;
 ?>
 <table class="table table-striped table-bordered" id="tb_map_truck">
     <thead>
@@ -23,11 +24,15 @@
                 <td><?php echo $rs['truck_1']; ?></td>
                 <td><?php echo $rs['truck_2']; ?></td>
                 <td>
-                    <?php if (!empty($rs['driver'])) { ?>
-                        <?php echo $rs['name'] . ' ' . $rs['lname']; ?>
-                    <button type="button" class="btn btn-warning btn-xs pull-right"
-                            onclick="dialog_driver('<?php echo $rs['car_id']; ?>')"><i class="fa fa-exchange"></i></button>
-                    <?php } else { ?>
+                    <?php
+                    if (!empty($rs['driver'])) {
+                        $driver_mode = new \app\models\Driver();
+                        $driver_id = $driver_mode->find()->where(['driver_id' => $rs['driver']])->one()['id'];
+                        ?>
+                        <a href="<?php echo Url::to(['driver/view', 'id' => $driver_id]) ?>" target="_bank"><?php echo $rs['name'] . ' ' . $rs['lname']; ?></a>
+                        <button type="button" class="btn btn-warning btn-xs pull-right"
+                                onclick="dialog_driver('<?php echo $rs['car_id']; ?>')"><i class="fa fa-exchange"></i></button>
+                            <?php } else { ?>
                         <div style="color: #ff0000;">
                             ไม่ได้เลือกคนขับ 
                             <button type="button" class="btn btn-default btn-sm pull-right" style=" padding: 0px 5px;"
@@ -36,9 +41,9 @@
                     <?php } ?>
                 </td>
                 <td style="text-align: center;">
-                    <a href="<?php echo Url::to(['map-truck/detail','car_id' => $rs['car_id']])?>"><i class="fa fa-eye"></i></a>&nbsp;
-                    <a href="<?php echo Url::to(['map-truck/update','car_id' => $rs['car_id']])?>"><i class="fa fa-edit"></i></a>&nbsp;
-                    <a href="<?php echo Url::to(['map-truck/delete','car_id' => $rs['car_id']])?>"><i class="fa fa-trash"></i></a>
+                    <a href="<?php echo Url::to(['map-truck/detail', 'car_id' => $rs['car_id']]) ?>"><i class="fa fa-eye"></i></a>&nbsp;
+                    <a href="<?php echo Url::to(['map-truck/update', 'car_id' => $rs['car_id']]) ?>"><i class="fa fa-edit"></i></a>&nbsp;
+                    <a href="<?php echo Url::to(['map-truck/delete', 'car_id' => $rs['car_id']]) ?>"><i class="fa fa-trash"></i></a>
                 </td>
             </tr>
         <?php endforeach; ?>
